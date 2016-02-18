d9794d306421f490b8c51e82cc8ffc73d30335c8
github username <mendelblesofsky@gmail.com>
2016-02-17 19:02:13 -0500

    small fix

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,10 @@
 class WelcomeController < ApplicationController
   def home
-  	Proccessed.destroy_all
-  	Website.destroy_all
   end
 
   def results
+  	Proccessed.destroy_all
+  	Website.destroy_all
   	website = Website.create(url: params[:url], depth_level: 1, title: params[:url])
   	Crawler.new(website, params[:depth].to_i, params[:time].to_i)
   end
